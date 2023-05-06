d312abf17e5d1fa5951cc3e9da625ce879177c37
Scott-Ramirez <anthonyscottramirez@gmail.com>
2023-05-06 15:32:02 -0500

    Nuevos cambios

--- AccesoDatos/CalificacionAD.cs
+++ AccesoDatos/CalificacionAD.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Entidades;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,54 @@ namespace AccesoDatos
 {
     public class CalificacionAD
     {
+        string conectionString = ConfigurationManager.ConnectionStrings["conx"].ConnectionString;
+        public int InsertarCalificacionAD(Calificaciones calificaciones)
+        {
+            int calificacionesid = 0;
+            // INSTANCIAMOS LA CONEXION A LA BD
+            using (SqlConnection sqlConnection = new SqlConnection(conectionString))
+            {
+                //LLAMAMOS AL SP DE LA BASE DE DATOS 
+                using (SqlCommand sqlCommand = new SqlCommand("SP_INSERTAR_EJERCICIO", sqlConnection))
+                {
+                    sqlCommand.CommandType = System.Data.CommandType.StoredProcedure;
+
+                    sqlCommand.Parameters.AddWithValue("@idAlumno", calificaciones.Id_Alumno);
+                    sqlCommand.Parameters.AddWithValue("@nombre", calificaciones.Nombre);
+                    sqlCommand.Parameters.AddWithValue("@realizado", calificaciones.Realizado);
+                    sqlCommand.Parameters.AddWithValue("@calificacion", calificaciones.Calificacion);
+                    sqlConnection.Open();
+                    calificacionesid = Convert.ToInt32(sqlCommand.ExecuteScalar());
+                    sqlConnection.Close();
+                }
+                return calificacionesid;
+            }
+        }
+
+        public List<Calificaciones> ListarCalificacionesAD()
+        {
+            List<Calificaciones> lista = new List<Calificaciones>();
+
+            using (SqlConnection sqlConnection = new SqlConnection(conectionString))
+            {
+                using (SqlCommand sqlCommand = new SqlCommand("ListarCalificaciones", sqlConnection))
+                {
+                    sqlCommand.CommandType = System.Data.CommandType.StoredProcedure;
+                    sqlConnection.Open();
+                    SqlDataReader reader = sqlCommand.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Calificaciones calificaciones = new Calificaciones();
+                        calificaciones.IdEjercicio = Convert.ToInt32(reader["IdEjercicio"]);
+                        calificaciones.Id_Alumno = Convert.ToInt32(reader["Id_Alumno"]);
+                        calificaciones.Nombre = Convert.ToString(reader["Nombre"]);
+                        calificaciones.Realizado = Convert.ToBoolean(reader["Realizado"]);
+                        calificaciones.Calificacion = Convert.ToInt32(reader["Calificacion"]);
+                        lista.Add(calificaciones);
+                    }
+                }
+            }
+            return lista;
+        }
     }
 }
---
+++ Entidades/Calificaciones.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Calificaciones
+    {
+        public int IdEjercicio { get; set; }
+        public int Id_Alumno { get; set; }
+        public string Nombre { get; set; }
+        public Boolean Realizado { get; set; }
+        public int Calificacion { get; set; }
+
+        public Calificaciones()
+        {
+            Id_Alumno = 0;
+            Nombre = "";
+            Realizado = false;
+            Calificacion = 0;
+        }
+    }
+}
